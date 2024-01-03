d63dc5816ad3460d44af1527b0959dccd3e5331b
dharmnath <surajshinde7888@gmail.com>
2024-01-03 14:56:51 +0530

    uploading..

---
+++ counter.js
@@ -0,0 +1,18 @@
+let number=0
+
+function increse(){
+   number=number+1;
+  
+  document.querySelector('.counte').innerHTML=number;
+}
+function reset
+()
+{
+  number=0;
+  document.querySelector('.counte').innerHTML=number;
+}
+function decrese(){
+  number=number-1;
+ 
+ document.querySelector('.counte').innerHTML=number;
+}
